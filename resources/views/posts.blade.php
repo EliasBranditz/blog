0c5bb2714a6f8f38079bf96bfcbf52b4228ced05
= <=>
2023-07-24 14:20:31 +0200

    Added my project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+<h1><a href="/post">My First Post</a></h1>
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget lectus in odio semper laoreet non et nibh. Duis eu consequat neque. Aenean quis urna condimentum, molestie sem in, imperdiet sem.
+</p>
+
+</article>
+
+<article>
+    <h1><a href="/post">My Second Post</a></h1>
+    
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget lectus in odio semper laoreet non et nibh. Duis eu consequat neque. Aenean quis urna condimentum, molestie sem in, imperdiet sem.
+</p>
+    
+</article>
+
+<article>
+<h1><a href="/post">My Third Post</a></h1>
+        
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget lectus in odio semper laoreet non et nibh. Duis eu consequat neque. Aenean quis urna condimentum, molestie sem in, imperdiet sem.
+</p>
+        
+</article>
+</body>
