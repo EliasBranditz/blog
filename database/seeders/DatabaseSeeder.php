a5324171458c3fddeeb4a676900e7fcb812ba0d0
= <=>
2023-08-02 16:23:12 +0200

    Worked with seeders and linking difrent tables to each other

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,66 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
+
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+/**
+ * Seed the application's database.
+ */
+public function run(): void
+{
+
+User::truncate();
+Post::truncate();
+Category::truncate();
+$user = User::factory()->create();
+
+$personal = Category::create([
+'name' => 'Personal',
+'slug' => 'personal'
+]);
+
+$family = Category::create([
+'name' => 'Family',
+'slug' => 'family'
+]);
+
+$work = Category::create([
+'name' => 'Work',
+'slug' => 'work'
+]);
+
+Post::create([
+'user_id' => $user->id,
+'category_id' => $family->id,
+'title' => 'My Family Post',
+'slug' => 'my-family-post',
+'exerpt' => '<p>Lorem ipsum dolor sit amet,</p>',
+'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed commodo urna. Nulla interdum consequat mauris quis scelerisque.</p>',
+]);
+
+Post::create([
+'user_id' => $user->id,
+'category_id' => $personal->id,
+'title' => 'My Personal Post',
+'slug' => 'my-personal-post',
+'exerpt' => '<p>Lorem ipsum dolor sit amet,</p>',
+'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed commodo urna. Nulla interdum consequat mauris quis scelerisque.</p>',
+]);
+
+Post::create([
+'user_id' => $user->id,
+'category_id' => $work->id,
+'title' => 'My Work Post',
+'slug' => 'my-work-post',
+'exerpt' => '<p>Lorem ipsum dolor sit amet,</p>',
+'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed commodo urna. Nulla interdum consequat mauris quis scelerisque.</p>',
+]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,18 @@
 <article class="{{ $loop->even ? 'foobar' : '' }}">
 <h1>
 <a href="/posts/{{ $post->slug }}">
-{{ $post->title }}
+{!! $post->title !!}
 </a>
 </a>    
 
 </h1>
 
+<p>
+<a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+</p>
+
 <div>
-{{ $post->exerpt; }}
+{!! $post->exerpt !!}
 </div>
 
 </article>
