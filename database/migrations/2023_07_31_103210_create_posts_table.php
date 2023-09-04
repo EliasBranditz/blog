2a65e858b40b4a50926e2235f6272015391d0404
= <=>
2023-09-04 11:10:16 +0200

    Install Framework

--- database/migrations/2023_07_31_103210_create_posts_table.php
+++ database/migrations/2023_07_31_103210_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('category_id');
             $table->string('slug')->unique();
             $table->string('title');
-            $table->string('excerpt');
+            $table->text('excerpt');
             $table->text('body');
             $table->timestamps();
             $table->timestamp('published_at')->nullable();
